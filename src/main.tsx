a0a573f22dda3ea273c7c053c600b3dc2edcd37b
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2026-01-28 12:35:26 +0100

    réalisation de la landing page version mobile

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import LandingPage from "./pages/LanndingPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <LandingPage />,
   },
 ]);
 
---
+++ src/pages/LanndingPage.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
+import logo_nplf from "../assets/Logo_neuvaine_rond_transparent.png";
+
+function LandingPage() {
+  return (
+    <div className="landing-div">
+      <figure className="landing-image-container">
+        <img className="landing-image" src={logo_nplf} alt='Logo de "Neuvaine pour la France' />
+      </figure>
+      <h1 className="landing-title">
+        Rejoignez-nous pour la prochaine neuvaine qui débutera le 22 mai 2026
+      </h1>
+      <Link to="/" className="enter-website">
+        Entrer sur le site
+      </Link>
+    </div>
+  );
+}
+
+export default LandingPage;
