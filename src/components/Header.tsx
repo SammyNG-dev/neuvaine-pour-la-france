0c881f7df2da30d48a61d5d2ce74e66bb96082d3
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2026-01-29 16:45:20 +0100

    barre de navigation pour la version desktop faite

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,22 @@ function Header() {
   return (
     <>
       <div className="header-head">
-        <img className="header-head-logo" src={nplf_logo} alt="Lofo de 'Neuvaine pour la France" />
+        <img
+          className="header-head-logo"
+          src={nplf_logo}
+          alt="Lofo de 'Neuvaine pour la France"
+        />
         <BurgerButton />
+        <nav className="header-navbar">
+          <ul className="menu-desktop">
+            <li className="header-desktop-link">L'esprit des neuvaines</li>
+            <li className="header-desktop-link">Prières pour la France</li>
+            <li className="header-desktop-link">Citations et testaments</li>
+            <li className="header-desktop-link">Groupe de prière</li>
+            <li className="header-desktop-link">Inscription</li>
+            <li className="header-desktop-link">Contact</li>
+          </ul>
+        </nav>
       </div>
       <div className="header-image-background">
         <img src={nplf_logo} className="header-image-logo" alt="" />
