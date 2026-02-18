9bc2bad3ddf10740f081b5f0f83205df48bda608
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2026-02-18 16:16:42 +0100

    affichage d'un saint et de sa prière

--- src/pages/SaintsGalery.tsx
+++ src/pages/SaintsGalery.tsx
@@ -1,5 +1,23 @@
-function SaintsGalery(){
-    return <h1>Saints Galery</h1>
+type SaintType = {
+  id: number;
+  name: string;
+  prayer: string;
+};
+
+const saint: SaintType = {
+  id: 1,
+  name: "Saint Thomas d'Aquin",
+  prayer: "Seigneur, guidez moi\ndans vos sentiers",
+};
+
+function SaintsGalery() {
+  return (
+    <>
+      <h1>Les saints patrons de France</h1>
+      <h2>{saint.name}</h2>
+      <p>{saint.prayer}</p>
+    </>
+  );
 }
 
-export default SaintsGalery
+export default SaintsGalery;
