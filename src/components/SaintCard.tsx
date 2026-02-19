a6dfabbd17ac1b04b4e46d9c263ab7d8222b74bf
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2026-02-19 13:47:08 +0100

    affichage d'une image d'un saint dans la galerie

--- src/components/SaintCard.tsx
+++ src/components/SaintCard.tsx
@@ -2,15 +2,17 @@ type SaintsType = {
   id: number;
   name: string;
   prayer: string;
+  img: string | null;
 };
 
 type SaintsTypeProps = {
-    saint: SaintsType
-}
+  saint: SaintsType;
+};
 
 function SaintCard({ saint }: SaintsTypeProps) {
   return (
     <>
+      {saint.img ? <img src={saint.img} alt={saint.name} /> : null}
       <h1>{saint.name}</h1>
       <p>{saint.prayer}</p>
     </>
--- src/pages/SaintsGalery.tsx
+++ src/pages/SaintsGalery.tsx
@@ -1,9 +1,11 @@
 import SaintCard from "../components/SaintCard";
+import therese_galery from "../assets/Sainte-Thérèse-galerie.jpg";
 
 type SaintsType = {
   id: number;
   name: string;
   prayer: string;
+  img: string | null;
 };
 
 const saints: SaintsType[] = [
@@ -11,24 +13,27 @@ const saints: SaintsType[] = [
     id: 1,
     name: "Saint Thomas d'Aquin",
     prayer: "Seigneur, guide moi dans tes sentiers",
+    img: null,
   },
   {
     id: 2,
     name: "Saint Anselme",
     prayer: "Seigneur, aide-moi !",
+    img: null,
   },
   {
     id: 3,
     name: "Sainte Thérèse de l'Enfant-Jésus et de la Sainte Face",
     prayer: "Dans le coeur de l'Eglise, ma mère, je serai l'amour",
+    img: therese_galery,
   },
 ];
 
 function SaintsGalery() {
   return (
     <>
-      {saints.map((saint)=> {
-        return <SaintCard key={saint.id} saint={saint}/>
+      {saints.map((saint) => {
+        return <SaintCard key={saint.id} saint={saint} />;
       })}
     </>
   );
