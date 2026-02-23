6563580f937c9406a12f42230b1590be3ee918ca
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2026-02-23 16:06:27 +0100

    Carte d'un saint mise en forme

--- src/components/SaintCard.tsx
+++ src/components/SaintCard.tsx
@@ -1,21 +1,26 @@
-type SaintsType = {
+import "./SaintCard.css";
+import { Link } from "react-router-dom";
+
+type SaintType = {
   id: number;
   name: string;
-  prayer: string;
   img: string | null;
 };
 
-type SaintsTypeProps = {
-  saint: SaintsType;
+type SaintTypeProps = {
+  saint: SaintType;
 };
 
-function SaintCard({ saint }: SaintsTypeProps) {
+function SaintCard({ saint }: SaintTypeProps) {
   return (
-    <>
-      {saint.img ? <img src={saint.img} alt={saint.name} /> : null}
-      <h1>{saint.name}</h1>
-      <p>{saint.prayer}</p>
-    </>
+    <div className="saint-card">
+      {saint.img ? (
+        <img className="saint-image" src={saint.img} alt={saint.name} />
+      ) : null}
+      <h3 className="saint-name">{saint.name}</h3>
+      <Link className="saint-button" to={`/page/prières-pour-la-france/${saint.id}`}>Prières pour la France</Link>
+      <Link className="saint-button" to={`/page/citations-et-testaments/${saint.id}`}>Citations et Testament</Link>
+    </div>
   );
 }
 
--- src/pages/SaintsGalery.tsx
+++ src/pages/SaintsGalery.tsx
@@ -4,7 +4,6 @@ import therese_galery from "../assets/Sainte-Thérèse-galerie.jpg";
 type SaintsType = {
   id: number;
   name: string;
-  prayer: string;
   img: string | null;
 };
 
@@ -12,19 +11,16 @@ const saints: SaintsType[] = [
   {
     id: 1,
     name: "Saint Thomas d'Aquin",
-    prayer: "Seigneur, guide moi dans tes sentiers",
     img: null,
   },
   {
     id: 2,
     name: "Saint Anselme",
-    prayer: "Seigneur, aide-moi !",
     img: null,
   },
   {
     id: 3,
     name: "Sainte Thérèse de l'Enfant-Jésus et de la Sainte Face",
-    prayer: "Dans le coeur de l'Eglise, ma mère, je serai l'amour",
     img: therese_galery,
   },
 ];
