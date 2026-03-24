cad28492ff77d2a2f639c2d465571501c98b9c71
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2026-03-24 12:44:09 +0100

    création du parser pour les textes

---
+++ src/utils/parseText.ts
@@ -0,0 +1,34 @@
+// src/utils/parseText.js
+
+import testamentsRaw from "../data/testaments.txt?raw";
+
+const testamentsSplitted = testamentsRaw.split("\n");
+
+type ObjectParsedTextType = {
+  id: number;
+  title: string;
+  content: string[];
+};
+
+function parseText(lines: string[], keyword: string) {
+  const list: ObjectParsedTextType[] = [];
+  let currentSection: ObjectParsedTextType | null = null;
+
+  lines.forEach((line) => {
+    const trimmed = line.trim();
+    if (trimmed.startsWith(keyword)) {
+      currentSection = {
+        id: list.length + 1,
+        title: trimmed,
+        content: [],
+      };
+      list.push(currentSection);
+    } else if (currentSection && trimmed !== "") {
+      currentSection.content.push(line);
+    }
+  });
+
+  return list
+}
+
+export const testamentsParsed = parseText(testamentsSplitted, "Testament");
